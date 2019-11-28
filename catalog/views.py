a408381b60873a541f8e16613c3116380154e88f
django_test <qhftkf1@naver.com>
2019-11-27 22:58:19 -0800

    First version

---
+++ catalog/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.decorators import permission_required
+
+# @permission_required('catalog.can_mark_returned')
+# @permission_required('catalog.can_edit')
+#
+# class MyView(PermissionRequiredMixin, View):
+#     permission_required = 'catalog.can_mark_returned'
+#     # Or multiple permissions
+#     permission_required = ('catalog.can_mark_returned', 'catalog.can_edit')
+#     # Note that 'catalog.can_edit' is just an example
+#     # the catalog application doesn't have such permission!
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
+
+# class MyView(PermissionRequiredMixin, View):
+#     permission_required = 'catalog.can_mark_returned'
+#     # Or multiple permissions
+#     permission_required = ('catalog.can_mark_returned', 'catalog.can_edit')
+#     # Note that 'catalog.can_edit' is just an example
+#     # the catalog application doesn't have such permission!
+
+class BookListView(generic.ListView):
+    model = Book
+    #  context_object_name = 'my_book_list'   # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, primary_key):
+        try:
+            book = Book.objects.get(pk=primary_key)
+        except Book.DoesNotExist:
+            raise Http404('Book does not exist')
+
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+    #
+    # def book_detail_view(request, primary_key):
+    #     book = get_object_or_404(Book, pk=primary_key)
+    #     return render(request, 'catalog/book_detail.html', context={'book': book})
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
