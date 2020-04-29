db3d1f4077492fc007a2063c1e85c10e8ba6b11d
qhftkf1 <qhftkfaos@gmail.com>
2020-04-29 11:31:54 +0900

    test

--- catalog/admin.py
+++ catalog/admin.py
@@ -18,7 +18,7 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstaceAdmin(admin.ModelAdmin):
      list_filter = ('status', 'due_back')
-     list_display = ('book','status','borrower', 'due_back', 'id')
+     list_display = ('book','status','borrower', 'due_back', 'isssd')
      fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
